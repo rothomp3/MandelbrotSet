dbfa27450bbfae8b951b121b28da7c143c852cf8
Robert Stephen Thompson <robert@Robert-Stephen-Thompsons-iMac.local>
2012-11-30 19:46:14 -0500

    Initial Commit

---
+++ MandelbrotSet/RTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTAppDelegate.h
+//  MandelbrotSet
+//
+//  Created by Robert Stephen Thompson on 11/30/12.
+//  Copyright (c) 2012 Robert Stephen Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MandelbrotSet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MandelbrotSet
+//
+//  Created by Robert Stephen Thompson on 11/30/12.
+//  Copyright (c) 2012 Robert Stephen Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+    }
+}
